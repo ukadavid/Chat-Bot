da66449c99d0fabe51bf31642f72995684903768
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-06-20 09:25:59 +0100

    more features on bot

--- my-chat-bot/bot.js
+++ my-chat-bot/bot.js
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const { ActivityHandler, MessageFactory, MemoryStorage, ConversationState } = require('botbuilder');
+const { ActivityHandler, ActionTypes, MessageFactory, CardFactory, MemoryStorage, ConversationState } = require('botbuilder');
 
 class EmailBot extends ActivityHandler {
     constructor() {
@@ -12,11 +12,11 @@ class EmailBot extends ActivityHandler {
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome! Please provide your email address:';
 
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                    await context.sendActivity('Welcome! Do you want to input your email?');
+                    await this.displayYesNoOptions(context);
                 }
             }
 
@@ -25,19 +25,24 @@ class EmailBot extends ActivityHandler {
 
         this.onMessage(async (context, next) => {
             const text = context.activity.text;
-
-            if (context.activity.channelData && context.activity.channelData.postBack) {
-                // Ignore postback activities
-                await next();
-                return;
-            }
-
             const conversationData = await this.conversationState.get(context, {});
-            if (!conversationData.email) {
+            const response = ['Yes', 'No'];
+
+            if (response.includes(text)) {
+                if (text === 'Yes' || text === 'Y') {
+                    await context.sendActivity('Please enter your email:');
+                } else if (text === 'No' || text === 'N') {
+                    await context.sendActivity('Thank you for your response, Goodbye!');
+                    await context.sendActivity({ type: 'endOfConversation' });
+                    return;
+                }
+            } else if (!conversationData.email) {
                 // If email is not set, validate and store the provided email
                 if (isValidEmail(text)) {
                     conversationData.email = text;
                     await context.sendActivity('Logged in successfully!');
+                    // Send the options card to the user
+                    await this.displayOptions(context);
                 } else {
                     // Show typing indicator before sending the error message
                     await context.sendActivity({ type: 'typing' });
@@ -45,7 +50,24 @@ class EmailBot extends ActivityHandler {
                 }
             } else {
                 // Email is already set, handle other messages here if needed
-                await context.sendActivity('You are already logged in.');
+                if (text === '1') {
+                    delete conversationData.email;
+                    await context.sendActivity('You have successfully logged out.');
+                    await context.sendActivity({ type: 'endOfConversation' });
+                    return;
+                } else if (text === '2') {
+                    const email = conversationData.email;
+                    const username = email.slice(0, email.indexOf('@'));
+                    await context.sendActivity(`Welcome to the dashboard, ${ username }@`);
+                } else if (text === '3') {
+                    const attachment = this.getInternetAttachment();
+                    const reply = MessageFactory.attachment(attachment);
+                    await context.sendActivity(reply);
+                    await context.sendActivity({ type: 'endOfConversation' });
+                    return;
+                } else {
+                    await context.sendActivity('Invalid option. Please select a valid option.');
+                }
             }
 
             await this.conversationState.set(context, conversationData); // Save conversation state
@@ -53,6 +75,54 @@ class EmailBot extends ActivityHandler {
 
             await next();
         });
+    }
+
+    async displayYesNoOptions(context) {
+        const cardActions = [
+            {
+                type: ActionTypes.ImBack,
+                title: 'Yes',
+                value: 'Yes',
+                style: 'positive', // Add this property for styling
+                image: 'https://cdn-icons-png.flaticon.com/512/6051/6051493.png', // Add an image for the button
+                imageAltText: 'Yes'// Alt text for the image
+            },
+            {
+                type: ActionTypes.ImBack,
+                title: 'No',
+                value: 'No',
+                style: 'negative', // Add this property for styling
+                image: 'https://www.counselmagazine.co.uk/images/uploadedfiles/2897df9f91d167a042c7a1058355e238561e.png?sfvrsn=c5df5f0a_3', // Add an image for the button
+                imageAltText: 'No' // Alt text for the image
+            }
+        ];
+        const reply = MessageFactory.suggestedActions(cardActions, 'Do you want to input your email?');
+        await context.sendActivity(reply);
+    }
+
+    async displayOptions(context) {
+        const reply = MessageFactory.attachment(
+            CardFactory.heroCard(
+                'Options',
+                undefined,
+                [
+                    { type: ActionTypes.ImBack, title: '1. Logout', value: '1' },
+                    { type: ActionTypes.ImBack, title: '2. Enter Dashboard', value: '2' },
+                    { type: ActionTypes.ImBack, title: '3. Finish', value: '3' }
+                ],
+                { text: 'Well done, it took you a great deal to be here' }
+            )
+        );
+
+        await context.sendActivity(reply);
+    }
+
+    getInternetAttachment() {
+        return {
+            name: 'architecture-resize.png',
+            contentType: 'image/png',
+            contentUrl: 'https://d1v7g7y4y70yfq.cloudfront.net/02-Blog/Main-Blog-Illustrations/blog-2020-04-07-how_to_say_thank_you_in_business.png'
+        };
     }
 }
 
